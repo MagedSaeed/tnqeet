e5d9e4a2b11359564d9e30275ceafaa9b705b351
magedsaeed <mageedsaeed1@gmail.com>
2026-04-06 10:54:44 +0300

    drop validation data from training data, retrain and run evaluations

--- tnqeet/data/__init__.py
+++ tnqeet/data/__init__.py
@@ -14,20 +14,29 @@ for i, example in enumerate(train_dataset.select(range(10_000))):  # type:ignore
 
 # Sample 15 examples from each source for each validation set
 val_indices = []
+llms_val_indices = []
 fewshot_val_indices = []
 
 for source, indices in source_groups.items():
-    # Take last 15 for val_dataset, first 15 for fewshot_val_dataset
-    val_indices.extend(indices[-15:])
+    # Take last 150 for val_dataset, first 15 for fewshot_val_dataset
+    val_indices.extend(indices[-150:])
+    llms_val_indices.extend(indices[-15:])
     fewshot_val_indices.extend(indices[:15])
 
 # Create the validation datasets
 val_dataset = train_dataset.select(val_indices)  # type:ignore
+llms_val_dataset = train_dataset.select(llms_val_indices)  # type:ignore
 fewshot_val_dataset = train_dataset.select(fewshot_val_indices)  # type:ignore
 
 # shuffle val datasets
 val_dataset = val_dataset.shuffle(seed=constants.RANDOM_SEED)
+llms_val_dataset = llms_val_dataset.shuffle(seed=constants.RANDOM_SEED)
 fewshot_val_dataset = fewshot_val_dataset.shuffle(seed=constants.RANDOM_SEED)
 
-# print(f"val_dataset size: {len(val_dataset)}")
+# Drop validation sets from train set to avoid data leakage
+all_val_indices = set(val_indices + fewshot_val_indices)
+train_indices = [i for i in range(len(train_dataset)) if i not in all_val_indices]  # type:ignore
+train_dataset = train_dataset.select(train_indices)  # type:ignore
+
+# print(f"llms_val_dataset size: {len(val_dataset)}")
 # print(f"fewshot_val_dataset size: {len(fewshot_val_dataset)}")
