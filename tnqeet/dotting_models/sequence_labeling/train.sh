ca76b901b73896412880aea1d300f09e4668ab6c
MagedSaeed <mageedsaeed1@gmail.com>
2026-04-13 15:20:01 +0300

    update training script to increase layer range and memory allocation

--- tnqeet/dotting_models/sequence_labeling/train.sh
+++ tnqeet/dotting_models/sequence_labeling/train.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-for num_layers in {1..5}
+mkdir -p tnqeet/dotting_models/sequence_labeling/training_logs
+
+for num_layers in {1..6}
 do
     sbatch --cpus-per-task=24 \
-           --mem=32G \
+           --mem=64G \
            --gres=gpu:1 \
-           --output=tnqeet/dotting_models/sequence_labeling/training_logs/slurm_run_${num_layers}.out \
-           --error=tnqeet/dotting_models/sequence_labeling/training_logs/slurm_run_${num_layers}.err \
-           --partition=A100 \
+           --partition=A6000 \
+           --output=tnqeet/dotting_models/sequence_labeling/training_logs/slurm_run_layers_${num_layers}_%j.out \
+           --error=tnqeet/dotting_models/sequence_labeling/training_logs/slurm_run_layers_${num_layers}_%j.err \
            --wrap="PYTHONUNBUFFERED=1 uv run python tnqeet/dotting_models/sequence_labeling/train.py --num_layers ${num_layers}"
 done
